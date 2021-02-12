3f5b84c3bb591c4c854d3ddf24a2be12b0d14fe8
野崎梨緒 <wada.rio1001@gmail.com>
2021-02-12 21:05:02 +0900

    Add form,search

--- hello.py
+++ hello.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-name = "page"
+name = "book"
 
 @app.route("/")
 def menu():
   return render_template("menu.html", name = name)
 
-@app.route("/get")
-def get():
-  message = name + "は、get"
-  return render_template("action.html", name = name, message = message)
+@app.route("/show")
+def show():
+  message = name + "の、post"
+  return render_template("show.html", name = name, message = message)
 
-@app.route("/post")
-def post():
-  message = name + "は、post"
-  return render_template("action.html", name = name, message = message)
+@app.route("/form")
+def form():
+  return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+  title = request.form["title"]
+  comment = request.form["comment"]
+  return render_template("show.html", title = title, comment = comment)
+
+@app.route("/search")
+def search():
+  words = request.args.get("words")
+  return render_template("search.html", words = words)
